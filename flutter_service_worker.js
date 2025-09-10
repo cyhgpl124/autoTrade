205b19cbded9bad11cf2e96db7bc7e2853394d03
cyhgpl124 <cyhgpl124@users.noreply.github.com>
2025-09-10 08:30:51 +0000

    deploy: d795aed6e90d1cc85dfc021e71cf1bd82423c2d0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -31,10 +31,10 @@ const RESOURCES = {"canvaskit/skwasm.js": "1ef3ea3a0fec4569e5d531da25f34095",
 "assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
-"flutter_bootstrap.js": "64678994146345e8053add9efc81ba26",
+"flutter_bootstrap.js": "806b9314ca7ce808622e50bee4a9d66a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"index.html": "aaa7a62bb45efb6b2543864f7eb9c8e2",
-"/": "aaa7a62bb45efb6b2543864f7eb9c8e2"};
+"index.html": "54a48d7e9c2dcff5248958535be8f617",
+"/": "54a48d7e9c2dcff5248958535be8f617"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
