fe121891e900da855d701e63e040dbaf567688b8
cyhgpl124 <cyhgpl124@users.noreply.github.com>
2025-09-11 04:45:09 +0000

    deploy: 313414d203f2b56376d8555c6a3001ab3d58f065

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1009664284"
+    serviceWorkerVersion: "788128956"
   }
 });
